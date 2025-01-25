12c1d4384badd7594e323b2f932d6869b88b3108
s30899 <s30899@pjwstk.edu.pl>
2025-01-25 10:44:21 +0100

    Start

---
+++ s30899Bank/s30899Bank/src/main/java/pl/pjatk/s30899Bank/S30899BankApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s30899Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S30899BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S30899BankApplication.class, args);
+	}
+
+}
---
+++ s30899Bank/s30899Bank/src/test/java/pl/pjatk/s30899Bank/S30899BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s30899Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S30899BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
